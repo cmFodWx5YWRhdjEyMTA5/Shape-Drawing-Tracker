74d0b24cbf7037c00de5b124371d9995a87b1efe
Rafael <rafael.alberti_fenrir@yahoo.fr>
2019-05-05 00:36:24 +0200

    Implemented the Shape object and the ShapeAdapter
    Need to fill the shape data with resource drawable and string value

--- app/src/main/java/com/umbral/www/shape_drawing_tracker/activities/ShapeSelectionActivity.kt
+++ app/src/main/java/com/umbral/www/shape_drawing_tracker/activities/ShapeSelectionActivity.kt
@@ -5,12 +5,16 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.umbral.www.shape_drawing_tracker.R
+import com.umbral.www.shape_drawing_tracker.adapters.ShapeAdapter
+import com.umbral.www.shape_drawing_tracker.models.Shape
 
 class ShapeSelectionActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
+
+    private val shapeData: ArrayList<Shape> = fillShapeDataSet()
 
     companion object {
         internal const val GRID_LAYOUT_SPAN_COUNT = 2
@@ -20,11 +24,12 @@ class ShapeSelectionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shape_selection)
 
-        viewManager = GridLayoutManager(this,
+        viewManager = GridLayoutManager(
+            this,
             GRID_LAYOUT_SPAN_COUNT
         )
 
-        //TODO Implement an Adapter for the RecyclerView
+        viewAdapter = ShapeAdapter(shapeData)
 
         recyclerView = findViewById<RecyclerView>(R.id.recycle_shape).apply {
             layoutManager = viewManager
@@ -33,6 +38,8 @@ class ShapeSelectionActivity : AppCompatActivity() {
 
     }
 
-
+    private fun fillShapeDataSet(): ArrayList<Shape> {
+        return ArrayList()
+    }
 
 }
--- app/src/main/java/com/umbral/www/shape_drawing_tracker/models/Shape.kt
+++ app/src/main/java/com/umbral/www/shape_drawing_tracker/models/Shape.kt
@@ -1,6 +1,12 @@
 package com.umbral.www.shape_drawing_tracker.models
 
+class Shape(var mShapeName: String, var mShapeImage: Int, var mShapeStatus: Boolean) {
 
-//TODO Implement Shape class
-class Shape {
+    /**
+    * Invert the status of the selected Shape
+    */
+    fun invertSelection() {
+        this.mShapeStatus = !mShapeStatus
+    }
+
 }
