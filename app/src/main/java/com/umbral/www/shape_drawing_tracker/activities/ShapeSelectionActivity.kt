4d43550dd90e6402da450ec1eba2a4c4ad0e31c0
Rafael <rafael.alberti.fenrir@gmail.com>
2019-05-05 22:34:08 +0200

    GridLayout of RecyclerView in ShapeSelection Activity is functional
    Need to add the drawables resources and the logic

--- app/src/main/java/com/umbral/www/shape_drawing_tracker/activities/ShapeSelectionActivity.kt
+++ app/src/main/java/com/umbral/www/shape_drawing_tracker/activities/ShapeSelectionActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.View
 import com.umbral.www.shape_drawing_tracker.R
 import com.umbral.www.shape_drawing_tracker.adapters.ShapeAdapter
 import com.umbral.www.shape_drawing_tracker.models.Shape
@@ -39,7 +41,15 @@ class ShapeSelectionActivity : AppCompatActivity() {
     }
 
     private fun fillShapeDataSet(): ArrayList<Shape> {
-        return ArrayList()
+        var shapeList: ArrayList<Shape> = ArrayList()
+        shapeList.add(Shape("Circle", R.drawable.shape_placeholder, false))
+        shapeList.add(Shape("Horizontal Oval", R.drawable.shape_placeholder, false))
+        shapeList.add(Shape("Vertical Oval", R.drawable.shape_placeholder, false))
+        shapeList.add(Shape("Diagonal Right Oval", R.drawable.shape_placeholder, false))
+        shapeList.add(Shape("Diagonal Left Oval", R.drawable.shape_placeholder, false))
+        shapeList.add(Shape("Thin Oval", R.drawable.shape_placeholder, false))
+        shapeList.add(Shape("Large Oval", R.drawable.shape_placeholder, false))
+        return shapeList
     }
 
 }
